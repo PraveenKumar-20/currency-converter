bcb2f65b561bb18d15b85ce0815604f90e20c876
Matpathi Praveen Kumar <praveenkumar1214241@gmail.com>
2024-05-21 18:33:12 +0530

    Small coding Challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+// https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("EUR");
+  const [output, setOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function getCurrencyDetails() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
+        );
+        const data = await res.json();
+        console.log(data);
+        // setOutput(Object.values(data.rates)[0]);
+        setOutput(data.rates[to]);
+        setIsLoading(false);
+      }
+
+      if (from === to) return setOutput(amount);
+      getCurrencyDetails();
+    },
+    [amount, from, to]
+  );
+
+  function handleInputAmount(e) {
+    setAmount(Number(e.target.value));
+  }
+
+  function handleFromInput(e) {
+    setFrom(e.target.value);
+  }
+
+  function handleToInput(e) {
+    setTo(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={handleInputAmount}
+        disabled={isLoading}
+      />
+      <select value={from} onChange={handleFromInput}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={to} onChange={handleToInput}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>
+        {output} {to}
+      </p>
     </div>
   );
 }
